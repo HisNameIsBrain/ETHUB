acd9a1e1364165ac70d01449de2b7d7fdda57f76
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-16 21:11:38 -0700

    update search-command.tsx

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -1,47 +1,80 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { File } from "lucide-react";
 import { useQuery } from "convex/react";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
-import { api } from "@/convex/_generated/api";
 import { useRouter } from "next/navigation";
-import { FileIcon } from "lucide-react";
-import { Id } from "@/convex/_generated/dataModel";
-import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 
-type ServiceType = {
-  _id: Id < "services" > ;
-  title: string;
-};
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { useSearch } from "@/hooks/useSearch";
+import { api } from "@/convex/_generated/api";
 
 export const SearchCommand = () => {
+  const { user } = useUser();
   const router = useRouter();
-  const [open, setOpen] = useState(false);
-  
-  const documents = useQuery(api.services.getSidebar) as ServiceType[] | undefined;
-  
-  const onSelect = (serviceId: Id < "services" > ) => {
-    setOpen(false);
-    router.push(`/services/${serviceId}`);
+  const documents = useQuery(api.documents.getSearch);
+  const [isMounted, setIsMounted] = useState(false);
+
+  const toggle = useSearch((store) => store.toggle);
+  const isOpen = useSearch((store) => store.isOpen);
+  const onClose = useSearch((store) => store.onClose);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        toggle();
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, [toggle]);
+
+  const onSelect = (id: string) => {
+    router.push(`/documents/${id}`);
+    onClose();
   };
-  
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <Command>
-      <CommandInput placeholder="Search services..." />
+    <CommandDialog open={isOpen} onOpenChange={onClose}>
+      <CommandInput placeholder={`Search ${user?.fullName}'s Zotion..`} />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Services">
-          {documents?.map((document: ServiceType) => (
+        <CommandGroup heading="Documents">
+          {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}-${document.title}`}
-              onSelect={() => onSelect(document._id)}
+              value={document._id}
+              title={document.title}
+              onSelect={onSelect}
             >
-              <FileIcon className="mr-2 h-4 w-4" />
-              {document.title}
+              {document.icon ? (
+                <p className="mr-2 text-[1.125rem]">{document.icon}</p>
+              ) : (
+                <File className="mr-2 h-4 w-4" />
+              )}
+              <span>{document.title}</span>
             </CommandItem>
           ))}
         </CommandGroup>
       </CommandList>
-    </Command>
+    </CommandDialog>
   );
 };
