0d533185a009c2f617871b8b8984d0a64d7ad4c6
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-22 19:27:26 +0000

    updatee services.ts

--- convex/functions/services.ts
+++
@@ -1,9 +0,0 @@
-import { query, mutation } from "convex/values";
-
-export const getById = query(({ db }, { id }) => {
-  return db.get(id);
-});
-
-export const update = mutation(({ db }, service) => {
-  return db.patch(service.id, service);
-});
--- convex/services.ts
+++ convex/services.ts
@@ -1,9 +1,64 @@
-import { query, mutation } from "convex/values";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
-export const getById = query(({ db }, { id }) => {
-  return db.get(id);
+export const getPublic = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("services").collect();
+  },
 });
 
-export const update = mutation(({ db }, service) => {
-  return db.patch(service.id, service);
+export const getById = query({
+  args: {
+    id: v.id("services"),
+  },
+  handler: async (ctx, { id }) => {
+    return await ctx.db.get(id);
+  },
+});
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+    description: v.string(),
+    price: v.number(),
+    deliveryTime: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("services", {
+      ...args,
+      createdAt: Date.now(),
+    });
+  },
+});
+
+export const update = mutation({
+  args: {
+    serviceId: v.id("services"),
+    name: v.string(),
+    description: v.string(),
+    price: v.number(),
+    deliveryTime: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.serviceId, {
+      name: args.name,
+      description: args.description,
+      price: args.price,
+      deliveryTime: args.deliveryTime,
+      updatedAt: Date.now(),
+    });
+  },
+});
+
+export const erase = mutation({
+  args: { serviceId: v.id("services") },
+  handler: async (ctx, { serviceId }) => {
+    await ctx.db.delete(serviceId);
+  },
+});
+
+export const listAll= query({
+  handler: async (ctx) => {
+    return await ctx.db.query("services").collect();
+  },
 });
