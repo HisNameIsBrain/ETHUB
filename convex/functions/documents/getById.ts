22e9a83b11c469aaaffcb3843fd4648c90210cff
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-27 16:35:28 -0700

    update

--- convex/functions/documents/getById.ts
+++
@@ -1,7 +0,0 @@
-import { query } from "convex/server";
-import { Id } from "../_generated/dataModel";
-
-export const getById = query(async ({ db }, args: { documentId: Id }) => {
-  const document = await db.get(args.documentId);
-  return document;
-});
