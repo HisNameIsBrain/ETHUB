05de3a3ff844b7a0474e502fbae1f3e99720ecfb
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-27 19:06:08 -0700

    update

--- convex/functions/documents/index.ts
+++ convex/functions/documents/index.ts
@@ -1,5 +1,6 @@
+// convex/functions/documents/index.ts (NEW)
 export * from "./getAll";
-export * from "./getById";
-export * from "./create";
-export * from "./update";
-export * from "./remove";
+export * from "./getDocumentById";
+export * from "./createDocument";
+export * from "./updateDocument";
+export * from "./removeDocument";
---
+++ convex/functions/services/index.ts
@@ -0,0 +1,6 @@
+// convex/functions/services/index.ts
+export * from "./getAll";
+export * from "./getServiceById";
+export * from "./createService";
+export * from "./updateService";
+export * from "./removeService";
