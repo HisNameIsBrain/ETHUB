6085cf537bd4bb4ff13d7c55e4d69e2c06d99ce7
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-28 02:36:52 +0000

    new funxs

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,18 +13,18 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as functions_documents_create from "../functions/documents/create.js";
+import type * as functions_documents_createDocument from "../functions/documents/createDocument.js";
 import type * as functions_documents_getAll from "../functions/documents/getAll.js";
-import type * as functions_documents_getById from "../functions/documents/getById.js";
+import type * as functions_documents_getDocumentById from "../functions/documents/getDocumentById.js";
 import type * as functions_documents_index from "../functions/documents/index.js";
-import type * as functions_documents_remove from "../functions/documents/remove.js";
-import type * as functions_documents_update from "../functions/documents/update.js";
-import type * as functions_services_create from "../functions/services/create.js";
+import type * as functions_documents_removeDocument from "../functions/documents/removeDocument.js";
+import type * as functions_documents_updateDocument from "../functions/documents/updateDocument.js";
+import type * as functions_services_createService from "../functions/services/createService.js";
 import type * as functions_services_getAll from "../functions/services/getAll.js";
-import type * as functions_services_getById from "../functions/services/getById.js";
-import type * as functions_services_remove from "../functions/services/remove.js";
+import type * as functions_services_getServiceById from "../functions/services/getServiceById.js";
+import type * as functions_services_removeService from "../functions/services/removeService.js";
 import type * as functions_services_services from "../functions/services/services.js";
-import type * as functions_services_update from "../functions/services/update.js";
+import type * as functions_services_updateService from "../functions/services/updateService.js";
 import type * as orders from "../orders.js";
 
 /**
@@ -36,18 +36,18 @@ import type * as orders from "../orders.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  "functions/documents/create": typeof functions_documents_create;
+  "functions/documents/createDocument": typeof functions_documents_createDocument;
   "functions/documents/getAll": typeof functions_documents_getAll;
-  "functions/documents/getById": typeof functions_documents_getById;
+  "functions/documents/getDocumentById": typeof functions_documents_getDocumentById;
   "functions/documents/index": typeof functions_documents_index;
-  "functions/documents/remove": typeof functions_documents_remove;
-  "functions/documents/update": typeof functions_documents_update;
-  "functions/services/create": typeof functions_services_create;
+  "functions/documents/removeDocument": typeof functions_documents_removeDocument;
+  "functions/documents/updateDocument": typeof functions_documents_updateDocument;
+  "functions/services/createService": typeof functions_services_createService;
   "functions/services/getAll": typeof functions_services_getAll;
-  "functions/services/getById": typeof functions_services_getById;
-  "functions/services/remove": typeof functions_services_remove;
+  "functions/services/getServiceById": typeof functions_services_getServiceById;
+  "functions/services/removeService": typeof functions_services_removeService;
   "functions/services/services": typeof functions_services_services;
-  "functions/services/update": typeof functions_services_update;
+  "functions/services/updateService": typeof functions_services_updateService;
   orders: typeof orders;
 }>;
 export declare const api: FilterApi<
