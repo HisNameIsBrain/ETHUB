043b5302b6fab934ea64194eeff957e04e4ee771
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-28 20:18:22 -0700

    update getDocumentById.ts

--- convex/functions/documents/getDocumentById.ts
+++ convex/functions/documents/getDocumentById.ts
@@ -1,7 +1,7 @@
 import { query, mutation } from "@/convex/_generated/server";
 import { v } from "convex/values";
 import type { MutationCtx, QueryCtx } from "@/convex/_generated/server";
-import type { Id } from "@/convex/_generated/dataModel";"
+import type { Id } from "@/convex/_generated/dataModel";
 
 
 export const getDocumentById = query({
