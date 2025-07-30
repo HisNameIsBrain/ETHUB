855ed8bb741f145c3d9204658fd2035d1b611ed7
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-07-30 16:29:50 -0700

    update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,4 +1,3 @@
-
 import { defineSchema, defineTable, v } from "convex/schema";
 
 export default defineSchema({
@@ -12,47 +11,44 @@ export default defineSchema({
     .index("by_email", ["email"]),
   
   documents: defineTable({
-    title: v.string(),
-    content: v.optional(v.string()),
-    coverImage: v.optional(v.string()),
-    icon: v.optional(v.string()), // 👈 Add this line
-    isArchived: v.boolean(),
-    isPublished: v.optional(v.boolean()),
-    parentDocument: v.optional(v.id("documents")),
-    orgId: v.optional(v.id("organizations")),
-    userId: v.string(),
-  })
+      title: v.string(),
+      content: v.optional(v.string()),
+      coverImage: v.optional(v.string()),
+      icon: v.optional(v.string()), // 👈 Add this line
+      isArchived: v.boolean(),
+      isPublished: v.optional(v.boolean()),
+      parentDocument: v.optional(v.id("documents")),
+      orgId: v.optional(v.id("organizations")),
+      userId: v.string(),
+    })
+    .index("by_org", ["orgId"])
+    .index("by_user", ["userId"]),
   
-  .index("by_org", ["orgId"])
-  .index("by_user", ["userId"]),
-
   organizations: defineTable({
     name: v.string(),
   }),
-
+  
   services: defineTable({
-    name: v.string(),
-    description: v.optional(v.string()),
-    type: v.optional(v.string()),
-    price: v.number(),
-    deliveryTime: v.string(),
-    createdAt: v.number(),
-    isArchived: v.boolean(),
-    orgId: v.optional(v.id("organizations")),
-  })
+      name: v.string(),
+      description: v.optional(v.string()),
+      type: v.optional(v.string()),
+      price: v.number(),
+      deliveryTime: v.string(),
+      createdAt: v.number(),
+      isArchived: v.boolean(),
+      orgId: v.optional(v.id("organizations")),
+    })
     .index("by_org", ["orgId"])
     .index("by_creation_time", ["_creationTime"]),
-})
-
+  
   orders: defineTable({
-    userId: v.string(),
-    serviceId: v.id("services"),
-    imei: v.string(),
-    serial: v.optional(v.string()),
-    status: v.string(),
-    notes: v.optional(v.string()),
-    createdAt: v.number(), // timestamp (Date.now())
-  })
-  
-  .index("by_user", ["userId"])
-  
+      userId: v.string(),
+      serviceId: v.id("services"),
+      imei: v.string(),
+      serial: v.optional(v.string()),
+      status: v.string(),
+      notes: v.optional(v.string()),
+      createdAt: v.number(), // timestamp (Date.now())
+    })
+    .index("by_user", ["userId"]),
+});
