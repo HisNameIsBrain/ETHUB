43399dfb4bc6cbd76829331119aac22422c9e8d9
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-08-01 02:08:57 -0700

    update use-scroll-top.tsx

--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,20 +1,18 @@
+"use client"; // 👈 This is required at the very top
+
 import { useState, useEffect } from "react";
 
 export const useScrollTop = (threshold = 10) => {
   const [scrolled, setScrolled] = useState(false);
-
+  
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > threshold) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrolled(window.scrollY > threshold);
     };
-
+    
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [threshold]);
-
+  
   return scrolled;
-}
+};
