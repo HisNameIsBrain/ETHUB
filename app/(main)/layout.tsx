42d0732f54e2bb4c3e27a27ac534c23e970b6bb0
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-08-01 11:10:08 -0700

    update layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,50 +1,44 @@
 "use client";
-
-import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexProviderWithAuth } from "convex/react";
-import { ConvexReactClient } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { useMemo } from "react";
-
 import { useConvexAuth } from "convex/react";
-import { redirect, usePathname } from "next/navigation";
+import { redirect } from "next/navigation";
+
 import { Spinner } from "@/components/spinner";
-import { Navigation } from "./_components/navigation";
 import { SearchCommand } from "@/components/search-command";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { Navigation } from "./_components/navigation";
+import { SiriGlow } from '@/components/siri-glow';
 
-export default function MainLayout({
-  children,
+
+
+const MainLayout = ({
+  children
 }: {
   children: React.ReactNode;
-}) {
+}) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
-  const pathname = usePathname();
 
   if (isLoading) {
     return (
-      <div className="h-screen w-full flex items-center justify-center bg-white dark:bg-black">
+      <div className="h-full flex items-center justify-center">
         <Spinner size="lg" />
       </div>
     );
   }
 
-  if (!isAuthenticated && pathname !== "/documents") {
-    redirect("/documents");
+  if (!isAuthenticated) {
+    return redirect("/");
   }
 
-  return (
-    <ClerkProvider>
-      <ConvexProviderWithAuth client={convex} useAuth={useConvexAuth}>
-        <div className="h-screen w-full flex flex-col dark:bg-[#1F1F1F]">
-          <Navigation />
-          <main className="flex-1 overflow-y-auto p-6">
-            <SearchCommand />
-            {children}
-          </main>
-        </div>
-      </ConvexProviderWithAuth>
-    </ClerkProvider>
-  );
+  return ( 
+    <div className="h-full flex dark:bg-[#1F1F1F]">
+     <SiriGlow />
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">
+      <SearchCommand />
+        {children}
+      </main>
+    </div>
+   );
 }
+ 
+export default MainLayout;
