b0300f7e06163ce542500470d2f23829101bb570
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-08-10 16:47:14 -0700

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,14 @@
-// app/layout.tsx
 import "./globals.css";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
-          <ConvexProvider client={convex}>
-            {children}
-          </ConvexProvider>
-        </ClerkProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
