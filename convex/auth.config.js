44aed50294ec782ca8b39c70a376a3c15f720343
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-08-14 02:54:31 +0000

    update auth.config.js

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // convex/auth.config.js
 export default {
   providers: [
@@ -7,3 +8,21 @@ export default {
     },
   ],
 };
+=======
+// convex/auth.config.ts
+import { ConvexAuth } from "convex/server";
+import { clerk } from "convex/nextjs";
+
+export default ConvexAuth({
+  providers: [
+    clerk({
+      // Make sure these environment variables are set in both Convex & local dev
+      // CLERK_JWT_ISSUER should be your Clerk instance domain, NOT your frontend URL
+      // Example: https://active-grizzly-98.clerk.accounts.dev
+      // CLERK_JWT_AUDIENCE should literally be "convex"
+      issuer: process.env.CLERK_JWT_ISSUER!,
+      audience: process.env.CLERK_JWT_AUDIENCE!,
+    }),
+  ],
+});
+>>>>>>> 101710d (update auth.config.js)
