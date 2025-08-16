03632a353ac2be0f36de7531a7e47c12b1330d10
HisNameIsBrain <earring.0.squalid@icloud.com>
2025-08-15 21:45:10 -0700

    update

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <SignIn
         routing="path"
-        path="/sign-in"
+        path="/dashboard"
         signInUrl="/sign-in"
         signUpForceRedirectUrl="/sign-in"
       />
